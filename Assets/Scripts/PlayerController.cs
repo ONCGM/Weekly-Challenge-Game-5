0573d9e1c22cd31a5a539ccec2b6f5a0989c2a59
ONCGM <oncgm@noreply.github.com>
2021-06-25 14:22:08 -0300

    movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour {
+    [Header("Settings")]
+    [SerializeField, Range(0.5f, 25f)] private float movementSpeed = 5f;
+    [SerializeField, Range(0.05f, 2f)] private float rotationSpeed = 0.5f;
+
+    private Vector2 movementDirection = Vector2.zero;
+    private Vector3 rotation = Vector3.zero;
+    
+    // Components.
+    private Rigidbody rb;
+
+    // Setup.
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // Input.
+    private void Update() {
+        if(Input.GetKey(KeyCode.W)) movementDirection += Vector2.up;
+        if(Input.GetKey(KeyCode.S)) movementDirection += Vector2.down;
         
+        if(Input.GetKey(KeyCode.D)) {
+            movementDirection += Vector2.right;
+            rotation = new Vector3(0f, 0f, -45f);
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            movementDirection += Vector2.left;
+            rotation = new Vector3(0f, 0f, 45f);
+        }
+    }
+
+    // Movement w/ physics.
+    private void FixedUpdate() {
+        rb.AddForce(movementDirection * movementSpeed, ForceMode.Force);
+        rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, Quaternion.Euler(rotation), rotationSpeed));
+        movementDirection = Vector2.zero;
+        rotation = Vector3.zero;
     }
 }
